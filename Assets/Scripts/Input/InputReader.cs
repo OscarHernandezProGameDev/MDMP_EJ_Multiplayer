5a7dac8622028c924a40e44eccf8d07a0c901cf4
bugbit <ohernandezba@gmail.com>
2024-05-01 11:33:17 +0200

    Leyendo controles

---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static Controls;
+
+[CreateAssetMenu(fileName = "New Input Reader", menuName = "Input/Input Reader")]
+public class InputReader : ScriptableObject, IPlayerActions
+{
+    private Controls controls;
+
+    #region Events
+    public event Action<bool> OnFireEvent;
+    public event Action<Vector3> OnMoveEvent;
+    #endregion
+
+    void OnEnable()
+    {
+        if (controls == null)
+        {
+            controls = new Controls();
+            controls.Player.SetCallbacks(this);
+        }
+
+        controls.Player.Enable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        OnMoveEvent?.Invoke(context.ReadValue<Vector3>());
+    }
+
+    public void OnFirePrimary(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            OnFireEvent?.Invoke(true);
+        }
+        else if (context.canceled)
+        {
+            OnFireEvent?.Invoke(false);
+        }
+    }
+}
