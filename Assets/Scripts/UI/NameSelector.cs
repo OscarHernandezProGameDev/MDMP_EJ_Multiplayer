45d836e50bd4b505491edd7ab57f5cd9ce27dfcb
Oscar Devs <ohernandezba@gmail.com>
2024-05-08 23:43:57 +0200

    Selección de nombre de jugador

---
+++ Assets/Scripts/UI/NameSelector.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NameSelector : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField nameField;
+    [SerializeField] private Button connectButton;
+    [SerializeField] private int minNameLength = 3;
+    [SerializeField] private int maxNameLength = 15;
+
+    private const string PlayerNameKey = "PlayerName";
+
+    public void HandleNameChanged()
+    {
+        connectButton.interactable = nameField.text.Length >= minNameLength && nameField.text.Length <= maxNameLength;
+    }
+
+    public void Connect()
+    {
+        PlayerPrefs.SetString(PlayerNameKey, nameField.text);
+
+        LoadNextScene();
+    }
+
+    void Start()
+    {
+        if (SystemInfo.graphicsDeviceType == UnityEngine.Rendering.GraphicsDeviceType.Null)
+        {
+            LoadNextScene();
+
+            return;
+        }
+
+        nameField.text = PlayerPrefs.GetString(PlayerNameKey, string.Empty);
+
+        HandleNameChanged();
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
