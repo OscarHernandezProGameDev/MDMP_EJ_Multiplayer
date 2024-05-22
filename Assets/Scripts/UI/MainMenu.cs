3c278ad05aee625cc10e075433007552d67d6903
Oscar Devs <ohernandezba@gmail.com>
2024-05-22 22:37:40 +0200

    UI de Matchmaking y configuración de Queue

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -10,6 +10,12 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private TMP_Text queueStatusText;
     [SerializeField] private TMP_Text queueStatusTimerText;
 
+    private void Start()
+    {
+        queueStatusText.text = string.Empty;
+        queueStatusTimerText.text = string.Empty;
+    }
+
     public async void StartHost()
     {
         await HostSingleton.Instance.GameManager.StartHostAsync();
