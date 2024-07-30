48d1c7ad8c4180c2c5e6b0e6bd8d57fa9f7e7327
Oscar Devs <ohernandezba@gmail.com>
2024-07-30 23:17:00 +0200

    Escribiendo el código de selección de personajes pt. 1

---
+++ Assets/Scripts/CaracterSelect/CaracterSelect.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class CaracterSelect : MonoBehaviour
+{
+    private NetworkObject SelectedCharacterPrefab;
+
+    [SerializeField] private GameObject[] characters;
+    [SerializeField] private int selectedCharacterIndex = 0;
+
+    public NetworkObject[] charactersPrefabs;
+
+    public void SelectNextCharacter()
+    {
+        characters[selectedCharacterIndex].gameObject.SetActive(false);
+        selectedCharacterIndex = (selectedCharacterIndex + 1) % characters.Length;
+        characters[selectedCharacterIndex].gameObject.SetActive(true);
+    }
+
+    public void SelectPreviousCharacter()
+    {
+        characters[selectedCharacterIndex].gameObject.SetActive(false);
+        selectedCharacterIndex--;
+        if (selectedCharacterIndex < 0)
+            selectedCharacterIndex = characters.Length - 1;
+        characters[selectedCharacterIndex].gameObject.SetActive(true);
+    }
+
+    public void ConfirmCharacter()
+    {
+        SelectedCharacterPrefab = charactersPrefabs[selectedCharacterIndex];
+    }
+
+    public string GetSelectdPrefabName()
+    {
+        return SelectedCharacterPrefab.name;
+    }
+}
